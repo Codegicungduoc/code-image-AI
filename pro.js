509b6dcc6cf5cd541d34225f048a910e3e6ca46d
Code gì cũng được <141378902+Codegicungduoc@users.noreply.github.com>
2023-10-06 15:28:37 +0700

    pro.js

--- pro.js
+++ pro.js
@@ -1,4 +1,4 @@
-const apiKey = "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";
+const apiKey = "KEY";
 
 const maxImages = 4; // Number of images to generate for each prompt
 let selectedImageNumber = null;
